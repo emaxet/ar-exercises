be8032f3ff0e44139e713062e6bbc8d8cdd3b2cf
Erik Tammpere <etammpere@gmail.com>
2017-12-06 06:46:14 +0000

    Added exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,6 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-
+@store2.employees.create(first_name: "Erik", last_name: "Tammpere", hourly_rate: 60)
+@store1.employees.create(first_name: "Elycia", last_name: "Tammpere", hourly_rate: 60)
+@store1.employees.create(first_name: "Nelson", last_name: "Tammpere", hourly_rate: 60)
